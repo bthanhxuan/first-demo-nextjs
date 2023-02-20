5a16e4f263c9d6daeb4d4f686f46c9848f81ff20
Bui Thanh Xuan <bthanhxuan12@gmail.com>
2023-02-20 10:22:15 +0700

    first-project-nextjs: b1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '../styles/Home.module.css';
+import React from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <>
       <Head>
@@ -119,5 +120,6 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
+export default Home;
